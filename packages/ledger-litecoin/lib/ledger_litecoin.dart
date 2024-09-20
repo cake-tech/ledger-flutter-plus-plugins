4577b2be6678d96a2130d67b982a387a66dd6965
Konstantin Ullrich <konstantinullrich12@gmail.com>
2024-09-20 16:14:30 +0200

    Clean up

--- packages/ledger-litecoin/lib/ledger_litecoin.dart
+++ packages/ledger-litecoin/lib/ledger_litecoin.dart
@@ -4,6 +4,3 @@ export 'src/ledger_litecoin_application.dart';
 export 'src/address_format.dart';
 export 'src/ledger_litecoin_exception.dart';
 export 'src/tx_utils/transaction.dart';
-
-// A collection of util extensions
-export 'src/utils/signature_uint8list_extension.dart';
